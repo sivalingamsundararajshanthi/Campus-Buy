02cbdbccb8b65ad26818f32d8233d4c4a22788d3
sivalingam <lingams200@gmail.com>
2019-03-02 19:17:39 -0600

    Initial Commit

---
+++ Campus Buy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Campus Buy
+//
+//  Created by siva lingam on 3/2/19.
+//  Copyright © 2019 NIU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
